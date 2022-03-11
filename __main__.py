d7aafcbf8ecbfe041c784bfb7667a168b626f9ce
Mighty <98230407+BeingMighty@users.noreply.github.com>
2022-03-12 00:19:51 +0530

    ^_^

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-# MightyXBotSpam || @MightyXSupport
+# MightyXSpam || @MightyXSupport
 
 import asyncio
 import sys
@@ -22,8 +22,8 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("Mighty X Bot Spam Successfully Deployed !!")
-print("Enjoy! Do Visit @MightyXSupport")
+print("Mighty X BotSpam Successfully Deployed !!")
+print("Enjoy..!  Do Visit @MightyXSupport")
 
 if __name__ == "__main__":
     Mig.run_until_disconnected()
